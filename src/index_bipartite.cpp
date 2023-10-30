dfef0e09243840a654cf2644d70fad52c6580abb
matchyc <dawnlight.yc@protonmail.com>
2023-10-31 01:22:08 +0800

    diff 10
    Signed-off-by: matchyc <dawnlight.yc@protonmail.com>

--- src/index_bipartite.cpp
+++ src/index_bipartite.cpp
@@ -802,7 +802,7 @@ void IndexBipartite::BuildGraphST2(size_t n_bp, const float *bp_data, Parameters
     assert(projection_ep_ < nd_);
     std::cout << "begin link projection" << std::endl;
     uint32_t pre_m = M_pjbp;
-    parameters.Set<uint32_t>("M_pjbp", M_pjbp - 5);
+    parameters.Set<uint32_t>("M_pjbp", M_pjbp - 10);
     LinkBase(parameters, simple_graph);
     parameters.Set<uint32_t>("M_pjbp", pre_m);
     // parameters.Set<uint32_t>("M_pjbp", static_cast<uint32_t>(pre_m * 1.5));
@@ -1988,7 +1988,7 @@ void IndexBipartite::LinkBase(const Parameters &parameters, SimpleNeighbor *simp
         // projection_graph_[i].clear();
         // supply_nbrs_[i] = projection_graph_[i];
         // supply_nbrs_[i].reserve(M_pjbp * 2 * PROJECTION_SLACK);
-        projection_graph_[i].reserve((M_pjbp + 5) * 2 * PROJECTION_SLACK);
+        projection_graph_[i].reserve((M_pjbp + 10) * 2 * PROJECTION_SLACK);
         // supply_nbrs_[i].reserve(M_pjbp * PROJECTION_SLACK);
         // supply_nbrs_[i].reserve(M_pjbp * PROJECTION_SLACK);
     }
